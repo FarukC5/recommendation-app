1dc414347118cc0446c57361419b429cb3825c95
FarukC5 <faruk_cajic@yahoo.com>
2022-05-23 03:25:57 +0200

    dialog modal fix

--- src/components/BookModal.js
+++ src/components/BookModal.js
@@ -2,13 +2,16 @@ import React from "react";
 import {
   Card,
   CardContent,
-  CardActions,
   Typography,
   Button,
   Link,
+  ButtonGroup,
+  Dialog,
+  DialogActions,
 } from "@mui/material";
+import noimage from "../images/noimage.png";
 
-const BookModal = ({ setOpen, book }) => {
+const BookModal = ({ setOpen, book, open }) => {
   const styles = {
     card: {
       textAlign: "center",
@@ -17,61 +20,75 @@ const BookModal = ({ setOpen, book }) => {
       left: "50%",
       transform: "translate(-50%, -50%)",
       width: "fit-content",
-      margin: "10px",
       boxShadow: "0px 0px 5px 0px ",
     },
 
     typograpfy: {
       cursor: "pointer",
     },
+    closeButton: {
+      margin: "5px",
+      textTransform: "none",
+    },
   };
 
   return (
     <div>
-      <Card style={styles.card}>
-        <CardContent>
-          <img
-            height="250"
-            src={book?.volumeInfo?.imageLinks?.thumbnail}
-            alt=""
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Title: {book.volumeInfo.title}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              Publisher: {book.volumeInfo.publisher}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              Author: {book.volumeInfo.authors}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              Page count: {book.volumeInfo.pageCount}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              Average rating: {book.volumeInfo.averageRating}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              Ratings count: {book.volumeInfo.ratingsCount}
-            </Typography>
-            <Link href={book.volumeInfo.canonicalVolumeLink}>
-              <Typography
-                style={styles.typograpfy}
-                gutterBottom
-                variant="h5"
-                component="div"
+      <Dialog open={open}>
+        <DialogActions>
+          <Card style={styles.card}>
+            <CardContent>
+              <img
+                height="250"
+                src={book?.volumeInfo?.imageLinks?.thumbnail || noimage}
+                alt=""
+              />
+
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h4"
+                  color="primary"
+                  component="div"
+                >
+                  {book.volumeInfo.title || "no info"}
+                </Typography>
+
+                <Typography gutterBottom variant="h5" component="div">
+                  {book.volumeInfo.authors || "no author"}
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Page count: {book.volumeInfo.pageCount || "no info"}
+                </Typography>
+
+                <Link href={book.volumeInfo.canonicalVolumeLink}>
+                  <Typography
+                    style={styles.typograpfy}
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                  >
+                    Read this book online
+                  </Typography>
+                </Link>
+              </CardContent>
+            </CardContent>
+
+            <ButtonGroup variant="contained" fullWidth color="primary">
+              <Button
+                style={styles.closeButton}
+                onClick={() => setOpen(false)}
+               // variant="outlined"
+                variant="outlined"
+                color="primary"
+                size="big"
               >
-                Read this book online
-              </Typography>
-            </Link>
-          </CardContent>
-        </CardContent>
-        <CardActions>
-          <Button onClick={() => setOpen(false)} size="small" color="primary">
-            Close
-          </Button>
-        </CardActions>
-      </Card>
+                Close
+              </Button>
+            </ButtonGroup>
+          </Card>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
