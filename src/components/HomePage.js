029c9da217cff9bae0a8d5d9093a404574483642
FarukC5 <faruk_cajic@yahoo.com>
2022-05-23 03:24:11 +0200

    mobile responsive and error msg

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SearchIcon from "@mui/icons-material/Search";
 import {
@@ -6,10 +6,10 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  InputBase,
   Button,
-  styled,
-  alpha,
+  Card,
+  CardContent,
+  TextField,
 } from "@mui/material";
 import BookList from "./BookList";
 import BookModal from "./BookModal";
@@ -19,21 +19,42 @@ const HomePage = () => {
   const [books, setBooks] = useState([]);
   const [recButton, setRecButton] = useState(false);
   const [open, setOpen] = useState(false);
+  const [openDetails, setOpenDetails] = useState(false);
+  const [errorMsg, setErrorMsg] = useState();
   const [book, setBook] = useState();
 
-  async function submitHandler(e) {
+  useEffect(() => {
+    document.addEventListener("mousedown", () => {
+      setOpenDetails(false);
+    });
+  });
+
+  function submitHandler(e) {
     e.preventDefault();
-    await axios
-      .get(`https://www.googleapis.com/books/v1/volumes?q=${input}`)
-      .then((result) => {
-        setBooks(result.data.items);
-        setRecButton(true);
-      });
+    if (input !== "") {
+      axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${input}`)
+        .then((result) => {
+          if (result.data.items !== undefined) {
+            setBooks(result.data.items);
+            setRecButton(true);
+          } else {
+            setErrorMsg("result not found!");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setErrorMsg("You did not enter anything!");
+      return;
+    }
   }
 
   const searchHandler = (e) => {
     e.preventDefault();
     setInput(e.target.value);
+    setErrorMsg("");
   };
 
   const recommendClick = () => {
@@ -42,109 +63,128 @@ const HomePage = () => {
   };
 
   const styles = {
+    appbar: {
+      borderRadius: "5px",
+    },
     box: {
       flexGrow: 1,
     },
-
+    bar: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    form: {
+      display: "flex",
+      alignItems: "center",
+      marginTop: "5px",
+    },
     typograpfy: {
-      flexGrow: 1,
       display: { xs: "none", sm: "block" },
     },
-
     div: {
       textAlign: "center",
+      margin: "2px",
     },
-
     button: {
-      marginTop: "10px",
-      width: "50%",
+      textTransform: "none",
+    },
+    error: {
+      display: "flex",
+      justifyContent: "center",
+    },
+    appbarCard: {
+      margin: "2px",
+    },
+    appbarContent: {
+      display: "flex",
+      justifyContent: "center",
+    },
+    modalCard: {
+      margin: "140px 5px 0px 5px",
+      background: "whitesmoke",
     },
   };
 
   return (
     <>
-      <div>
-        <Box style={styles.box}>
-          <AppBar position="static">
-            <Toolbar style={styles.bar}>
-              <Typography
-                style={styles.typograpfy}
-                variant="h5"
-                noWrap
-                component="div"
-              >
-                Book Recommender
-              </Typography>
-              <form onSubmit={submitHandler}>
-                <Search>
-                  <SearchIconWrapper>
-                    <SearchIcon />
-                  </SearchIconWrapper>
-                  <StyledInputBase
-                    placeholder="Search titles…"
+      <>
+        <AppBar color="primary" style={styles.appbar}>
+          <Card color="primary">
+            <div style={styles.appbarCard}>
+              <Box style={styles.box}>
+                <Card>
+                  <Toolbar style={styles.bar}>
+                    <Typography
+                      style={styles.typograpfy}
+                      variant="h5"
+                      noWrap
+                      component="div"
+                    >
+                      Book Recommender
+                    </Typography>
+                    <Typography variant="subtitle12">
+                      Which book should you read?
+                    </Typography>
+                  </Toolbar>
+                </Card>
+
+                <form onSubmit={submitHandler} style={styles.form}>
+                  <TextField
+                    fullWidth
+                    size="small"
+                    placeholder=" Enter the book title"
                     onChange={searchHandler}
                   />
-                </Search>
-              </form>
-            </Toolbar>
-          </AppBar>
-        </Box>
-      </div>
-      {!!recButton && (
-        <div style={styles.div}>
-          <Button
-            onClick={recommendClick}
-            variant="contained"
-            style={styles.button}
-          >
-            Recommend me a book
-          </Button>
-          <BookList setOpen={setOpen} setBook={setBook} books={books} />
-        </div>
-      )}
-      {!!open && <BookModal setOpen={setOpen} book={book} />}
+                  <Button variant="outlined" onClick={submitHandler}>
+                    <SearchIcon />
+                  </Button>
+                </form>
+
+                {!!errorMsg && (
+                  <Typography
+                    color="red"
+                    variant="subtitle1"
+                    style={styles.error}
+                  >
+                    {errorMsg}
+                  </Typography>
+                )}
+              </Box>
+            </div>
+
+            {recButton && books ? (
+              <div style={styles.div}>
+                <Button
+                  fullWidth
+                  onClick={recommendClick}
+                  variant="outlined"
+                  color="primary"
+                  style={styles.button}
+                >
+                  Recommend
+                </Button>
+              </div>
+            ) : (
+              <Card style={styles.appbarCard}>
+                <AppBar position="relative">
+                  <CardContent style={styles.appbarContent}>
+                    <Typography variant="subtitle12">
+                      Search the books and chose recomanded
+                    </Typography>
+                  </CardContent>
+                </AppBar>
+              </Card>
+            )}
+          </Card>
+        </AppBar>
+      </>
+      <Card style={styles.modalCard}>
+        {recButton && books && <BookList books={books} />}
+
+        {!!open && <BookModal open={open} setOpen={setOpen} book={book} />}
+      </Card>
     </>
   );
 };
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "20ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 
 export default HomePage;
